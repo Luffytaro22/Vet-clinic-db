668327d23a0d061ff8b2df24359040b003a49984
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-14 14:22:38 -0500

    Add the sql code for creating the table

--- schema.sql
+++ schema.sql
@@ -3,3 +3,16 @@
 CREATE TABLE animals (
     name varchar(100)
 );
+
+postgres=# CREATE DATABASE vet_clinic;
+CREATE DATABASE
+postgres=# \c vet_clinic
+
+vet_clinic=# CREATE TABLE animals(
+vet_clinic(# id         INT,
+vet_clinic(# name       varchar(40),
+vet_clinic(# date_of_birth      date,
+vet_clinic(# escape_attempts    INT,
+vet_clinic(# neutered   bool,
+vet_clinic(# weight_kg  decimal(5, 2)
+vet_clinic(# );
