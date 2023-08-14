e38d58335af3e8f2786b87c3c7af1ed30259d8d7
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-14 15:03:31 -0500

    Add SQL code for queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,12 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name = 'Luna';
+
+vet_clinic=# SELECT * FROM animals WHERE name LIKE '%mon';
+vet_clinic=# SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+vet_clinic=# SELECT name fROM animals WHERE neutered = true AND escape_attempts < 3;
+vet_clinic=# SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+vet_clinic=# SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+vet_clinic=# SELECT * FROM animals WHERE neutered = true;
+vet_clinic=# SELECT * FROM animals WHERE NOT name = 'Gabumon';
+vet_clinic=# SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
