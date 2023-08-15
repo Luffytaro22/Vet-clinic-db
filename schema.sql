d461f1858a7e333f43a36b9768d6c132bc3aa292
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-15 10:23:41 -0500

    Add a new species column in animals table

--- schema.sql
+++ schema.sql
@@ -18,3 +18,6 @@ vet_clinic(# weight_kg          decimal(5,2)
 vet_clinic(# ,
 vet_clinic(# PRIMARY KEY(id)
 vet_clinic(# );
+
+vet_clinic=# ALTER TABLE animals
+vet_clinic-# ADD species        varchar(50);
