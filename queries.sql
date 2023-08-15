631f9a39716a6c4f5fb1a592460ce7d204f1dd7a
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-15 10:48:46 -0500

    Modify the species column inside a transaction and rollback

--- queries.sql
+++ queries.sql
@@ -10,3 +10,12 @@ vet_clinic=# SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 vet_clinic=# SELECT * FROM animals WHERE neutered = true;
 vet_clinic=# SELECT * FROM animals WHERE NOT name = 'Gabumon';
 vet_clinic=# SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+* Update species column with unspecified and roll back to the previous state:
+vet_clinic=# BEGIN;
+BEGIN
+vet_clinic=*# UPDATE animals
+vet_clinic-*# SET species = 'unspecified';
+UPDATE 11
+vet_clinic=*# ROLLBACK;
+ROLLBACK
